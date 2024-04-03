f8166d32f6102c2842bc2f5adf7f1510a0273334
Lupin The Destructor <lupin@Lupins-MBP.lan>
2024-04-03 13:16:15 -0400

    First commit.

---
+++ src/test/java/org/learn/amigo/AmigoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.learn.amigo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmigoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
