fbff04e1759dbcde63118552e8e0330a5ef49e44
Lupin The Destructor <lupin@Lupins-MBP.lan>
2024-04-03 14:19:10 -0400

    Adds Main.java with SpringBoot

---
+++ src/main/java/com/learn/Main.java
@@ -0,0 +1,14 @@
+package com.learn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import javax.swing.*;
+
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
+
